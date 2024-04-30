4300fc5c77bdbabd7e59b1d10aa713fa420d829a
Muhammad Umar <157218505+umarfareed3452@users.noreply.github.com>
2024-04-30 17:19:54 +0500

    Add files via upload

---
+++ loginform.php
@@ -0,0 +1,34 @@
+<?php 
+
+$host="localhost";
+$user="root";
+$password="";
+$db="demo";
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="div">
+    <img src="avatardefault_92824.png" alt="">
+    <form method="POST" action="#">
+        
+    <div class="form_input">
+<input type="text" name="User name" placeholder="Enter your user name">
+
+    </div>
+    <div class="form_input">
+        <input type="password" name="password" placeholder="Enter your password ">
+    </div>
+    </form>
+<input type="submit"  name="submit" value="LOGIN" class="btn-login">
+    </div>
+</body>
